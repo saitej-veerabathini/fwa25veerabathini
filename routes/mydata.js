5d48efe87cbac4c08a0e35b1c9ca640064ee2c79
saitej-veerabathini <saitej.veerabathini@gmail.com>
2021-10-27 22:52:40 -0500

    rectified errors

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Saitej Veerabathini' });
+  res.render('mydata', {title: 'Saitej Veerabathini' });
 });
 
 module.exports = router;
