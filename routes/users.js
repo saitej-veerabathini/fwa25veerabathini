6e0e41f9cdec723b4cc3f1b3dde0a64643c72350
saitej-veerabathini <saitej.veerabathini@gmail.com>
2021-10-27 22:11:52 -0500

    modified user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var index =0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  index++;
+  res.send(`User accesses are: ${index}`);
 });
 
 module.exports = router;
